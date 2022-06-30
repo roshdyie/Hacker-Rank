b80407b6847c6e4b85c8df2f55ad1e5c51a87065
Amrr Roshdyie <103902225+roshdyie@users.noreply.github.com>
2022-06-30 03:44:48 +0200

    Create Select All.sql

---
+++ SQL/Select All.sql
@@ -0,0 +1,9 @@
+-- We have 2 solutions
+-- The first one :
+
+SELECT *
+FROM CITY;
+
+-- The secound One and It's better for the performance
+SELECT ID, NAME, COUNTRYCODE, DISTRICT, POPULATION
+FROM CITY;
